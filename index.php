75d66cddde6fad0d962c9e55414c3d6bf7591a49
Grant Bartlett <grant@smswmedia.com>
2015-03-19 17:14:09 +0000

    modernizr

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --><!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 <body <?php if($locale->getLanguage() == 'ar_AR') echo 'class="rtl"';?>>
 
