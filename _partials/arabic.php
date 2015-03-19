8c5ec3ea4862e6da39494bf562d001dde33a62b1
Grant Bartlett <grant@smswmedia.com>
2015-03-19 11:51:48 +0000

    Partials for eng/arabic content

---
+++ _partials/arabic.php
@@ -0,0 +1,47 @@
+                    <section class="intro text-center">
+                        <h1 class="text-uppercase">Hey You</h1>
+
+                        <p class="lead">We've been waiting!</p>
+
+                        <p class="description">
+                            Don't miss out for a single second longer.<br class="hidden-xs"> Promotions, contests, style guides and more, coming your way...
+                        </p>
+                    </section>
+
+                    <section class="form">
+                        <div class="row">
+                            <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+
+                                <form action="https://smswmedia.createsend.com/t/d/s/iljdlk/" method="post">
+                                    <div class="row">
+                                        <div class="form-group col-sm-6 col-md-6">
+                                            <input id="fieldjyurdy" name="cm-f-jyurdy" type="text" class="form-control" placeholder="first name" autofocus>
+                                        </div>
+                                        <div class="form-group col-sm-6 col-md-6">
+                                            <input id="fieldjyurdj" name="cm-f-jyurdj" type="text" class="form-control" placeholder="last name">
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-md-12">
+                                            <input type="email" id="fieldEmail" name="cm-iljdlk-iljdlk" class="form-control" placeholder="email" required/>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-md-6 col-md-offset-3 text-center">
+                                            <button class="btn btn-default btn-block btn-lg btn-primary text-uppercase" type="submit">I'm In</button>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </section>
+
+                    <footer class="footer text-center">
+                        <p>
+                            <a href="#" title="">Terms and conditions</a>,
+                            <a href="#" role="button" data-toggle="modal" data-target="#privacyPolicy">Privacy Policy</a>
+                        </p>
+                    </footer>
