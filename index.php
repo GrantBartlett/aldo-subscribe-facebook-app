da5206aabb27b4db81db5af2f511a39cb843a79c
Grant Bartlett <grant@smswmedia.com>
2015-03-23 10:12:24 +0000

    IE <9 doesn't read ://

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
         </div>
     </section>
 </div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="dist/js/app.min.js"></script>
 </body>
 </html>
